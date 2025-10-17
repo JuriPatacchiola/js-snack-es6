f85f5d6c0def94455b243ee03eb381d2aff6c94a
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-17 18:09:49 +0200

    Add the new array

--- slack2/assets/js/script.js
+++ slack2/assets/js/script.js
@@ -1,4 +1,4 @@
-// Creo l'array iniziale con i nomi delle squadre
+    // Creo l'array iniziale con i nomi delle squadre
     const squadre = [
       { nome: "Juventus", punti: 0, falliSubiti: 0 },
       { nome: "Inter", punti: 0, falliSubiti: 0 },
@@ -7,11 +7,14 @@
       { nome: "Roma", punti: 0, falliSubiti: 0 }
     ];
 
-     // Assegno numeri casuali a punti e falli
+    // Assegno numeri casuali a punti e falli
     for (let i = 0; i < squadre.length; i++) {
       squadre[i].punti = Math.floor(Math.random() * 100); // punti casuali
       squadre[i].falliSubiti = Math.floor(Math.random() * 50); // falli casuali
     }
 
-     console.log("Tutte le squadre con dati casuali:");
+    console.log("Tutte le squadre con dati casuali:");
     console.log(squadre);
+
+    // Creo un nuovo array con solo nome e falliSubiti
+    const nuovoArray = [];
