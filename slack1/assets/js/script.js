2302e348dd768fe8e0e720af8e0f952e4f0fc7ee
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-17 18:02:11 +0200

    add the searche cile is small havy

--- slack1/assets/js/script.js
+++ slack1/assets/js/script.js
@@ -1,7 +1,15 @@
- // mi creo un array di oggetti, ognuno rappresenta una bici
+ // Mi creo un array di oggetti, ognuno rappresenta una bici
     const bici = [
       { nome: "Pinarello Dogma F", peso: 6.8 },
       { nome: "Specialized Tarmac SL8", peso: 6.5 },
       { nome: "Canyon Ultimate CFR", peso: 6.7 },
       { nome: "Trek Emonda SLR", peso: 6.4 },
     ];
+
+     // Cerco lala bici con peso minore
+    let biciLeggera = bici[0];
+    for (let i = 1; i < bici.length; i++) {
+      if (bici[i].peso < biciLeggera.peso) {
+        biciLeggera = bici[i];
+      }
+    }
