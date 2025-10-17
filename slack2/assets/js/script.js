529d1635823c0678bddfa6bb58d2630f26120c40
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-17 18:06:28 +0200

    add the ather array object for the second exercise

---
+++ slack2/assets/js/script.js
@@ -0,0 +1,8 @@
+// Creo l'array iniziale con i nomi delle squadre
+    const squadre = [
+      { nome: "Juventus", punti: 0, falliSubiti: 0 },
+      { nome: "Inter", punti: 0, falliSubiti: 0 },
+      { nome: "Milan", punti: 0, falliSubiti: 0 },
+      { nome: "Napoli", punti: 0, falliSubiti: 0 },
+      { nome: "Roma", punti: 0, falliSubiti: 0 }
+    ];
