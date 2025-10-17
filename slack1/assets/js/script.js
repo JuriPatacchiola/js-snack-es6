019bf393b2435a175e4db5c912d456e13d3199bc
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-10-17 18:01:00 +0200

    add the array object

---
+++ slack1/assets/js/script.js
@@ -0,0 +1,7 @@
+ // mi creo un array di oggetti, ognuno rappresenta una bici
+    const bici = [
+      { nome: "Pinarello Dogma F", peso: 6.8 },
+      { nome: "Specialized Tarmac SL8", peso: 6.5 },
+      { nome: "Canyon Ultimate CFR", peso: 6.7 },
+      { nome: "Trek Emonda SLR", peso: 6.4 },
+    ];
